79fba901448cec24d147fd212f4b14fab9e67ea0
Dmitriy NetoIT <pr0100_smile@mail.ru>
2025-03-11 22:53:34 +0300

    Repair

--- src/js/__tests__/goblin.test.js
+++ src/js/__tests__/goblin.test.js
@@ -2,8 +2,8 @@ import GameBoard from '../gameBoard';
 
 
 test('формирование игрового поля', () => {
-  const board = new Board();
-  const boards = board.gameBoard(4);
+  const gameBoard = new GameBoard();
+  const boards = gameBoard.gameBoard(4);
   const received = boards.querySelectorAll('.cell');
   expect(received.length).toBe(4 ** 2);
 });
